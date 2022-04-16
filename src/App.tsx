064b0d50e24d3d7fc863621b0f27e9c7db722a98
Eric Le Donge <eric.ledonge@lacapitale.com>
2022-04-15 22:21:28 -0400

    :tada:

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import './App.css';
-
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Hello from './components/Hello';
+import Navbar from './components/navbar';
+import Explore from './pages/explore';
+import ForgotPassword from './pages/forgot-password';
+import Offers from './pages/offers';
+import SignIn from './pages/sign-in';
+import SignUp from './pages/sign-up';
 
-function App() {
-  return <Hello name="Éric" />;
-}
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+
+        <Navbar />
+      </Router>
+    </>
+  );
+};
 
 export default App;
